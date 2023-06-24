1ba9164ed7620df557d0042a34141df58ec704ae
LeoGuo621 <335209779@qq.com>
2023-06-25 05:07:36 +0800

    create handler and router

--- httpserver/src/main.rs
+++ httpserver/src/main.rs
@@ -1,3 +1,10 @@
+mod handler;
+mod router;
+mod server;
+
+use server::{self, Server}:Server;
+
 fn main() {
-    println!("Hello, world!");
+    let server = Server::new("localhost:3000");
+    server.run();
 }
